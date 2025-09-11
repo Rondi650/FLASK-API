1b0215c20b89c4dc4b5b2cc5756a16e2596be28c
Rondi650 <rondinellenunes123@gmail.com>
2025-09-11 17:29:18 -0300

    CRUD terminado

--- app/models/products.py
+++ app/models/products.py
@@ -20,3 +20,9 @@ class ProductDBModel(Product):
         if self.id:
             data["_id"] = str(data["_id"])
         return data
+    
+class UpdateProduct(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    description: Optional[str] = None
+    stock: Optional[int] = None
--- app/routes/main.py
+++ app/routes/main.py
@@ -77,14 +77,36 @@ def get_product_by_id(product_id):
         return jsonify({"error": f"Produto com o id: {product_id} - Não encontrado"})
     
 # RF: O sistema deve permitir a atualizacao de um unico produto e produto existente
-@main_bp.route('/product/<int:product_id>', methods=['PUT'])
-def update_product(product_id):
-    return jsonify({"message":f"Esta é a rota de atualizacao do produto com o id {product_id}"})
+@main_bp.route('/product/<string:product_id>', methods=['PUT'])
+@token_required
+def update_product(token, product_id):
+    try:
+        oid = ObjectId(product_id)
+        update_data = UpdateProduct(**request.get_json())
+        update_result = db.products.update_one(
+            {"_id": oid},
+            {"$set": update_data.model_dump(exclude_unset=True)}
+        )
+        if update_result.matched_count == 0:
+            return jsonify({"error": "Produto não encontrado"}), 404
+        updated_product = db.products.find_one({"_id": oid})
+        return jsonify(ProductDBModel(**updated_product).model_dump(by_alias=True, exclude_none=True))
+    except ValidationError as e:
+        return jsonify({"error": e.errors()})
+
 
 # RF: O sistema deve permitir a delecao de um unico produto e produto existente
-@main_bp.route('/product/<int:product_id>', methods=['DELETE'])
-def delete_product(product_id):
-    return jsonify({"message":f"Esta é a rota de deleção do produto com o id {product_id}"})
+@main_bp.route('/product/<string:product_id>', methods=['DELETE'])
+@token_required
+def delete_product(token, product_id):
+    try:
+        oid = ObjectId(product_id)
+        delete_product = db.products.delete_one({"_id": oid})
+        if delete_product.deleted_count == 0:
+            return jsonify({"error": "Produto não foi encontrado"}), 404
+        return "", 204
+    except Exception:
+        return jsonify({"error": "id do produto inválido"}), 400
 
 # RF: O sistema deve permitir a importacao de vendas através de um arquivo
 @main_bp.route('/sales/upload', methods=['POST'])
